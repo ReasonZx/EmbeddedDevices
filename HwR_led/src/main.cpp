c35668bf2e9656ac1d418ef843150b45e7232bd1
Xico <xico.reasonzx@gmail.com>
2021-02-11 18:12:53 +0000

    Connecting ESP8266 to wifi through serial monitor

--- HwR_led/src/main.cpp
+++ HwR_led/src/main.cpp
@@ -1,25 +1,66 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <string.h>
 
-const int LED   = 1; // Assigning Pin 4 as the name LED
-const int LED2  = 4; // Assigning Pin 4 as the name LED
+
+const int LED   = 1; // Assigning Pin 1 as the name LED
+String ssid;
+String pass;
+
 void setup ()  
 {
-     pinMode (LED, OUTPUT); // Declaring LED pin as an output.
-     pinMode (LED2, OUTPUT); // Declaring LED pin as an output.   
-     //Serial.begin(115200);  
+    delay(3000);
+    Serial.begin(9600);    
+    Serial.println("Connecting to wifi");
+    Serial.print("SSID: ");
+    char incomingByte = 0;
+    
+    while(1) {
+        if (Serial.available() > 0) {
+            incomingByte = Serial.read();
+            if(incomingByte == 13) break;
+            Serial.print(incomingByte);
+            ssid.concat(static_cast<char>(incomingByte));
+        }
+    }
+
+    Serial.println();
+    delay(1000);
+    Serial.print("PASS: ");
+    incomingByte = 0;
+    while (Serial.available() > 0) incomingByte = Serial.read();        //clean the Serial Buffer
+
+    while(1) {
+        if (Serial.available() > 0) {
+            incomingByte = Serial.read();
+            if(incomingByte == 13) break;
+            Serial.print("*");
+            pass.concat(static_cast<char>(incomingByte));
+        }
+    }
+    
+    WiFi.begin(ssid,pass);
+
+    Serial.print("\nConnecting");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
+    
 }
 
 
 
 void loop () // Code under this loop runs forever.
 {
-    digitalWrite (LED, HIGH); // Making LED High.
-    digitalWrite (LED2, HIGH); // Making LED High. 
-    //Serial.println("HI ");  
+    //digitalWrite (LED, HIGH); // Making LED High.
     delay(1000);              // Some Delay
-    digitalWrite (LED, LOW);  // Making LED LOW.
-    digitalWrite (LED2, LOW); // Making LED High.
-    //Serial.println("LO ");  
-    delay(2000);              // Some Delay
+    //digitalWrite (LED, LOW);  // Making LED LOW.
+    //delay(2000);              // Some Delay
+    Serial.println("Looping");  
 }
